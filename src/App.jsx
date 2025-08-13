fb892c56d008d45a7ff750abe7c369678d1a33e1
Aayush Shrestha <learn.aayush@gmail.com>
2025-08-13 22:13:15 +0545

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect, useState } from 'react'
 import './App.css'
+import {Children} from './components/Children'
+import { News } from './components/News'
+import { Link, Route, Routes } from 'react-router-dom'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+ const [name,setName] =useState('riwaj') 
+
+ const [count,setCount] = useState(1)
+
+
 
   return (
-   
-      <div>
-        
-        </div>
-        
+    <>
+
+<nav className='flex gap-4  p-4'>
+  
+  <Link to ="home">Home </Link> 
+  <Link to ="news">News </Link> 
+  <Link to ="contact">Contact </Link> 
+</nav>
+
+
+  <Routes>
+    <Route path='/home' element={<Children/>}></Route>
+    <Route path='/news' element={<News/>}></Route>
+    <Route path='/contact' element={<Children/>}></Route>
+  </Routes>
+
+ 
+     {/* <News/> */}
+    </>
   )
 }
 
